435094490922904981869267230f3045412f0be2
Daniel Schulz <schulz@sensape.com>
2021-12-09 18:26:44 +0100

    feat: custom font

--- components/head.js
+++ components/head.js
@@ -1,10 +1,16 @@
-import { useRouter } from "next/router";
 import NextHead from "next/head";
 
 export default function Head() {
   return (
     <NextHead>
       <title>Hochzeit F&F</title>
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Proza+Libre&display=swap"
+        rel="stylesheet"
+      />
     </NextHead>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["monospace", "sans-serif"],
+      sans: ["Proza Libre", "sans-serif"],
       serif: ["Merriweather", "serif"],
     },
     extend: {
