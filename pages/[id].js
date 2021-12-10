4d8c80d181adb4346d546658e9d342acea814894
Daniel Schulz <schulz@sensape.com>
2021-12-10 23:36:12 +0100

    chore: download styled

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,10 @@
+import { useState } from "react";
 import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
 import files from "data/files";
 import { nanoid } from "nanoid";
 import readSubfolders from "utils/readSubfolders";
-import { FiArrowUp } from "react-icons/fi";
-import { Button } from "components/buttons";
+import { FiArrowDown, FiArrowUp } from "react-icons/fi";
 import { useCallback } from "react";
 import { download } from "utils/download";
 
@@ -47,13 +47,18 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function Abends({ images, title, cdn, bucket }) {
-  const handleDownload = useCallback(async (key) => {
+  const [isLoading, setIsLoading] = useState([]);
+
+  const handleDownload = useCallback(async (e, key) => {
+    setIsLoading((old) => [...old, key]);
     const res = await fetch(`api/image?key=${key}`).then((res) => res.json());
     const { protocol, pathname, search } = new URL(res.link);
     const blob = await fetch(
       `${protocol}//${bucket}.${cdn}${pathname}${search}`
     ).then((res) => res.blob());
     download(URL.createObjectURL(blob), key);
+    e.target.blur();
+    setIsLoading((old) => old.filter((k) => k !== key));
   }, []);
 
   return (
@@ -74,32 +79,47 @@ export default function Abends({ images, title, cdn, bucket }) {
         </div>
       </a>
       <div className="mt-4 pb-4 md:container mx-4 md:mx-auto">
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="shadow-lg mb-4 bg-white animate-fadeIn md:w-2/3 lg:w-1/2 mx-auto p-2"
-          >
-            <div className="relative block overflow-hidden">
-              <div
-                className="absolute top-0 left-0 w-full h-full right-0 bottom-0 scale-150 blur-2xl"
-                style={{
-                  ...image.css,
-                }}
-              />
-              <Image {...image.img} sizes="100% " layout="responsive" />
-              <div
-                className="absolute top-0 left-0 w-full h-full right-0 bottom-0 bg-gray-50/50"
-                // onClick={() =>
-                //   handleDownload(
-                //     image.href
-                //       .replace(/\/images\//, "")
-                //       .replace(/.webp/, ".jpg")
-                //   )
-                // }
-              ></div>
+        {images.map((image) => {
+          const key = image.href
+            .replace(/\/images\//, "")
+            .replace(/.webp/, ".jpg");
+          return (
+            <div
+              key={image.id}
+              className="shadow-lg mb-4 bg-white animate-fadeIn md:w-2/3 lg:w-1/2 mx-auto p-2"
+            >
+              <div className="relative block overflow-hidden">
+                <div
+                  className="absolute top-0 left-0 w-full h-full right-0 bottom-0 scale-150 blur-2xl"
+                  style={{
+                    ...image.css,
+                  }}
+                />
+                <Image {...image.img} sizes="100% " layout="responsive" />
+                <div
+                  tabIndex={0}
+                  className="transition-opacity absolute top-0 left-0 w-full h-full right-0 bottom-0 opacity-0 flex focus:opacity-100  md:hover:opacity-100  bg-wedding-dark/50 items-center justify-center"
+                >
+                  <button
+                    className={`flex flex-col gap-1 align-middle pointer-events-none peer-focus-within:pointer-events-auto`}
+                    onClick={(e) => handleDownload(e, key)}
+                  >
+                    <span
+                      className={`flex justify-center text-6xl text-white p-1 ${
+                        isLoading.some((x) => x === key) && "animate-ping"
+                      }`}
+                    >
+                      <FiArrowDown />
+                    </span>
+                    <div className="text-white">
+                      Click nochmal und das Foto fliegt zu dir.
+                    </div>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <a
